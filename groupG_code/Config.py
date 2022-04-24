9ddd774d0314475dc7d33d85b5dd32a10519c354
Hubert <hubert.wojcik002@gmail.com>
2022-04-24 12:47:33 +0200

    uploaded the exam submission

---
+++ groupG_code/Config.py
@@ -0,0 +1,15 @@
+
+images_path = './../data/example_image'
+mask_path = './../data/example_segmentation'
+
+#150 dataset
+features_path = 'features.csv'
+example_ground_truth_path = 'example_ground_truth.csv'
+
+#2k dataset
+male_features_path = '2kmale_features.csv'
+female_features_path = '2kfemale_features.csv'
+sex_features_path = 'sex_features.csv'
+
+#our guesses in a csv file
+id_rating_path = './../groupG_ratings/ID_rating.csv'
